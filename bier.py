52aa7091fa0d6ac3ed5f04e6810dcc73539bc231
Mike Luttikhuis <mikeluttikhuis@gmail.com>
2019-06-08 16:24:55 +0200

    Thx @wouteroonk

--- bier.py
+++ bier.py
@@ -4,60 +4,62 @@ import re
 import smtplib
 import os
 
-winkels = [
-			'Albert Heijn',
-			'Gall En Gall',
-			'Coop Supermarkt',
-			'JUMBO'
-			]
+shops = [
+                        'Albert Heijn',
+                        'Gall En Gall',
+                        'Coop Supermarkt',
+                        'JUMBO'
+                        ]
 
 base_url = 'https://www.biernet.nl/bier/aanbiedingen/'
-merk_url = [
-			'grolsch-premium-pilsener/krat-van-24-flesjes-a-030-liter',
-			'grolsch-premium-pilsener/krat-van-16-flesjes-a-045-liter',
-			'hertog-jan-pilsener/krat-van-24-flesjes-a-030-liter',
-			'warsteiner-premium-pilsener/krat-van-24-flesjes-a-030-liter'
-			]
-output = []
+brands_url = [
+                        'grolsch-premium-pilsener/krat-van-24-flesjes-a-030-liter',
+                        'grolsch-premium-pilsener/krat-van-16-flesjes-a-045-liter',
+                        'hertog-jan-pilsener/krat-van-24-flesjes-a-030-liter',
+                        'warsteiner-premium-pilsener/krat-van-24-flesjes-a-030-liter'
+                        ]
+beer_discount = []
 
-for merk in merk_url:
-	r = requests.get(base_url+merk)
-	c = r.content
-	soup = BeautifulSoup(c, "lxml")
+for brand in brands_url:
+        r = requests.get(base_url+brand)
+        c = r.content
+        soup = BeautifulSoup(c, "lxml")
 
-	aanbieding = soup.find('ul', {"class":"aanbiedingen"})
-	text_aanbieding = aanbieding.find_all('div', {"class":"textaanbieding"})
-	for winkel in text_aanbieding:
-		if winkel.find('p', {"class":"Kratten"}) is not None and winkel.find('img')['title'].title() in winkels:
-			winkel_prijs = {}
-			winkel_prijs['naam'] = winkel.find('img')['title'].title()
-			winkel_prijs['prijs'] = winkel.find('span', {"class":"prijs"}).text
-			winkel_prijs['merk'] = winkel.find('span', {"class":"merke"}).text
-			winkel_prijs['product'] = winkel.find('p', {"class":"Kratten"}).text
-			winkel_prijs['geldig'] = re.sub(r"\s+", " ", winkel.find('p', {"style":"margin:0;"}).text.strip())
-			output.append(winkel_prijs)
+        discount = soup.find('ul', {"class":"aanbiedingen"})
+        textdiscount = discount.find_all('div', {"class":"textaanbieding"})
+        for shop in textdiscount:
+                if shop.find('p', {"class":"Kratten"}) is not None and shop.find('img')['title'].title() in shops:
+                        shop_info = {}
+                        shop_info['name'] = shop.find('img')['title'].title()
+                        shop_info['price'] = shop.find('span', {"class":"prijs"}).text
+                        shop_info['brand'] = shop.find('span', {"class":"merke"}).text
+                        shop_info['product'] = shop.find('p', {"class":"Kratten"}).text
+                        shop_info['valid'] = re.sub(r"\s+", " ", shop.find('p', {"style":"margin:0;"}).text.strip())
+                        beer_discount.append(shop_info)
 
-output = sorted(output, key=lambda k: k['naam'])
-txtbody = open('body.txt', 'w')
-for i in range(0,len(output)):
-	txtbody.write('Winkel:\t'+output[i]['naam']+'\n')
-	txtbody.write('Merk:\t'+output[i]['merk']+'\n')
-	txtbody.write('Prijs:\t'+(output[i]['prijs'])+' - '+output[i]['product']+'\n')
-	txtbody.write('Geldig:\t'+output[i]['geldig']+'\n')
-	txtbody.write('\n')
+beer_discount = sorted(beer_discount, key=lambda k: k['price'], reverse=True)
+
+def generatebody():
+        body = []
+        for i in range(0,len(beer_discount)):
+                body.append('Winkel:\t'+beer_discount[i]['name']+'\n')
+                body.append('Merk:\t'+beer_discount[i]['brand']+'\n')
+                body.append('Prijs:\t'+(beer_discount[i]['price'])+' - '+beer_discount[i]['product']+'\n')
+                body.append('Geldig:\t'+beer_discount[i]['valid']+'\n')
+                body.append('\n')
+        return ''.join(body)
 
 gmail_user = os.environ['gmail_user']
 gmail_password = os.environ['gmail_pass']
 
 sent_from = os.environ['gmail_user']
-to = os.environ['gmail_pass']
+to = os.environ['gmail_user']
 subject = 'Bier aanbiedingen - Daily'
-txtbody = open('body.txt', 'r')
-body = txtbody.read()
+body = generatebody()
 
 email_text = """\
-From: %s  
-To: %s  
+From: %s
+To: %s
 Subject: %s
 
 %s
@@ -67,4 +69,4 @@ server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 server.ehlo()
 server.login(gmail_user, gmail_password)
 server.sendmail(sent_from, to, email_text.encode('utf8'))
-server.close()
+server.close
